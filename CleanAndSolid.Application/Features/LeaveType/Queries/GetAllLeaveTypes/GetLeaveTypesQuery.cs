2c7339d6b3dafd0c728992901f6ab93e110680b3
Audrey457 <laudrey457@gmail.com>
2024-02-06 16:31:19 +0100

    CQRS query et handler : GetLeaveTypes

---
+++ CleanAndSolid.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CleanAndSolid.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    //On peut utiliser record car ce sera immutable.
+    public record GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>;
+}
---
+++ CleanAndSolid.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CleanAndSolid.Application.Contracts.Persistance;
+using MediatR;
+
+namespace CleanAndSolid.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+    {
+        private readonly IMapper mapper;
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this.mapper = mapper;
+            this.leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+            //Query the database
+            var leaveTypes = await leaveTypeRepository.GetAsync();
+
+            //Convert data objects to Dto Objects
+            var data = mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+            //return list of DTO Objet
+            return data;
+        }
+    }
+}
