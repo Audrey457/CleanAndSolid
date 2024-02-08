10d7514767c5d0bafba9c9c8ab5390ee3a3bb1f1
Audrey457 <laudrey457@gmail.com>
2024-02-08 14:35:31 +0100

    Mise en place persistence

---
+++ CleanAndSolid.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanAndSolid.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+    }
+}
