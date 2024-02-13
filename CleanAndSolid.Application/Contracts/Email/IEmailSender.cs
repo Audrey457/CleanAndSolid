c9e85768da85df9e19f987468cf7703719beb646
Audrey457 <laudrey457@gmail.com>
2024-02-13 17:47:28 +0100

    ajout d'un service tiers pour l'email

---
+++ CleanAndSolid.Application/Contracts/Email/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using CleanAndSolid.Application.Models.Email;
+
+namespace CleanAndSolid.Application.Contracts.Email
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(EmailMessage email);
+    }
+}
--- CleanAndSolid.Infrastructure/InfrastructureServicesRegistration.cs
+++ CleanAndSolid.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,8 +1,10 @@
 ﻿
 
+using CleanAndSolid.Application.Contracts.Email;
+using CleanAndSolid.Application.Models.Email;
+using CleanAndSolid.Infrastructure.EmailService;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.CompilerServices;
 
 namespace CleanAndSolid.Infrastructure
 {
@@ -10,6 +12,9 @@ namespace CleanAndSolid.Infrastructure
     {
         public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            //Transient car on veut une nouvelle instance de EmailSender à chaque fois qu'on l'utilise.
+            services.AddTransient<IEmailSender, EmailSender>();
             return services;
         }
     }
