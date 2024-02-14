657d5ab1dd34921fe70dd5368f0e51f564623715
Audrey457 <laudrey457@gmail.com>
2024-02-14 12:46:42 +0100

    configurer le projet api

--- CleanAndSolid.Api/Program.cs
+++ CleanAndSolid.Api/Program.cs
@@ -1,8 +1,22 @@
+using CleanAndSolid.Application;
+using CleanAndSolid.Infrastructure;
+using CleanAndSolid.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationsServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 builder.Services.AddControllers();
+
+//Très important pour pouvoir utiliser cette application avec des services et clients externes.
+//Avec cette policy, on accordera à tous d'appeler l'API avec n'importe quel type de header, n'importe quelle type de méthode
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("all", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
